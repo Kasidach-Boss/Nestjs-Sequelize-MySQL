69a7ccb4ec7383bece51a6793f7a0226fa853fc3
Kasidach-Boss <kasidachst@gmail.com>
2021-02-09 00:40:54 +0700

    first commit

---
+++ src/users/user.model.ts
@@ -0,0 +1,13 @@
+import {AutoIncrement, Column, Model, Table} from 'sequelize-typescript';
+
+@Table
+export class User extends Model<User> {
+    @Column
+    firstName: string;
+  
+    @Column
+    lastName: string;
+  
+    @Column({ defaultValue: true })
+    isActive: boolean;
+  }
